c10140c3bc2e83eb25b2e163dc740da7ded5e556
hah5158 <hah5158@calu.edu>
2016-02-28 15:22:22 -0500

    Update captureImage.py

--- BAD/captureImage.py
+++ BAD/captureImage.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 import os
 import time
+import threading
 
 
 # Home cherokee directory:
@@ -16,26 +17,25 @@ import time
 #-----------------------------------------------------------------------
 
 class CaptureImage:
-    imageID = "frame.jpeg"
-    targetID = "target.png"
-    gopro = UC()
-    path = "/media/external/"
 
     def __init__(self):
         # Make sure the GoPro is turned on, initialize settings and begin capture.
+        self.imageID = "frame.jpeg"
+        self.targetID = "target.png"
+        self.goproURL = UC()
+        self.path = os.getcwd()
+        print self.path
         print "Initializing GoPro.."
         #self.gopro.turn_on()
-        #self.begin_capture()
-
+        
     def start_photo_thread(self):
         # capture thread to capture photos during flight
-        self.gopro.start_capture()
-        self.gopro.stop_capture()
+        print "Started " + threading.currentThread().getName() + " " + str(time.time())
 
     def start_search_thread(self):
         # thread to handle the URL requests, we dont want them to cause the captures to fall behind
-        self.get_photo()
-        
+        print "Started " + threading.currentThread().getName() + " " + str(time.time())
+
     def capture_video(self):
         self.gopro.enable_camera_mode()  # enable camera video mode
         self.gopro.start_capture()
@@ -45,10 +45,11 @@ class CaptureImage:
         self.imageID = self.gopro.get_image_id()
 
     def begin_capture(self):
+        print "Init Threading.."
         # Init all of the GoPro Settings to capture a photo
-        self.gopro.enable_photo_mode()
-        self.gopro.start_capture()
-        self.gopro.stop_capture()
+        # self.gopro.enable_photo_mode()
+        # self.gopro.start_capture()
+        # self.gopro.stop_capture()
         self.start_search_thread()
         self.start_photo_thread()
         
@@ -56,19 +57,22 @@ class CaptureImage:
         self.gopro.turn_off()
 
 cap = CaptureImage()
-
+t1 = threading.Thread(name="capture_thread", target=cap.start_photo_thread)
+t2 = threading.Thread(name="cherokee_thread", target=cap.start_search_thread)
+t1.start()
+t2.start()
 # Collect the images captured
 os.chdir(cap.path)
-img1 = cv2.imread(cap.targetID,0)	# Target image from user.
+img1 = cv2.imread(cap.targetID,0)   # Target image from user.
 os.chdir(cap.path + "/img")
-img2 = cv2.imread(cap.imageID,0)	# Current frame from GoPro
+img2 = cv2.imread(cap.imageID,0)    # Current frame from GoPro
 
 t1 = time.time()
 
 # Create the SURF object for keypoints and descriptors
 surf = cv2.xfeatures2d.SURF_create(400)
-kp1, des1 = surf.detectAndCompute(img1,None)	# Keypoints, Target
-kp2, des2 = surf.detectAndCompute(img2,None)	# Keypoints, Frame
+kp1, des1 = surf.detectAndCompute(img1,None)    # Keypoints, Target
+kp2, des2 = surf.detectAndCompute(img2,None)    # Keypoints, Frame
 
 # FLANN parameters needed for flann matcher
 FLANN_INDEX_KDTREE = 0
@@ -87,7 +91,7 @@ good = []
 for i,(m,n) in enumerate(matches):
      if m.distance < 0.6*n.distance:
          matchesMask[i]=[1,0]
-         good.append(m)	# Append the good match for count/comparison
+         good.append(m) # Append the good match for count/comparison
  
 # Adjust the drawing parameters
 draw_params = dict(matchColor = (0,255,0),
@@ -98,7 +102,7 @@ draw_params = dict(matchColor = (0,255,0),
 # Draw the image found and save to hits directory
 hit = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
 print (str(time.time()-t1) + " s " + 
-				"\nTotal Matches: "+ 
-				str(len(matches)))
+                "\nTotal Matches: "+ 
+                str(len(matches)))
 os.chdir(cap.path + "/hits")
 cv2.imwrite("hit.jpeg", hit)
