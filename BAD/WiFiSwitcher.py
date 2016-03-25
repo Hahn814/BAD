7f45f6a34791dbe9fdc58d0dc326bad124affdc5
Paul Hahn <paulhahn18@gmail.com>
2016-03-25 10:50:17 -0400

    Update WiFiSwitcher.py

--- BAD/WiFiSwitcher.py
+++ BAD/WiFiSwitcher.py
@@ -2,12 +2,12 @@ from urlCommands import UrlCommands as UC
 import fileinput
 import os
 
-fileLocation = "/etc/network/"      # Directory of the wifi configuration file
-fileToSearch = "interfaces"         # Wifi file name
-searchText1 = "wpa-ssid"            # tags to find correct line within file
-searchText2 = "wpa-psk"             # .
-goProWiFi = "wpa-ssid " + UC.ip     # replacement lines
-goProPsk = "wpa-psk " + UC.pw       # .
+fileLocation    = "/etc/network/"       # Directory of the wifi configuration file
+fileToSearch    = "interfaces"          # Wifi file name
+searchText1     = "wpa-ssid"            # tags to find correct line within file
+searchText2     = "wpa-psk"             # .
+goProWiFi       = "wpa-ssid " + UC.ip   # replacement lines
+goProPsk        = "wpa-psk " + UC.pw    # .
 
 temp = os.getcwd()                  # capture original directory to return to afterward
 os.chdir(fileLocation)
