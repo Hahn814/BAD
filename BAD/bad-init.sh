33306d34140c2e2c5171276b1d79eff9ec630a72
Paul Hahn <paulhahn18@gmail.com>
2016-03-25 13:02:49 -0400

    Update bad-init.sh

--- BAD/bad-init.sh
+++ BAD/bad-init.sh
@@ -1,1 +1,6 @@
 echo "This shell command has not been updated yet, disable driver, ensure GoPro WiFi, run captureImage.py"
+echo "Change directory"
+echo "run switcher python script - connect to wifi network for ftp"
+echo "run ftp script to retrieve target"
+echo "run switcher python script - switch to gopro wifi"
+echo "run capture image script"
