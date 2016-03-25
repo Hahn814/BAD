48f868c40e6483b1b3407b07f4d25fa17d70b551
Paul Hahn <paulhahn18@gmail.com>
2016-03-25 13:37:13 -0400

    Create acquireTargetImage.py

---
+++ BAD/acquireTargetImage.py
@@ -0,0 +1,18 @@
+import ftplib as FTP
+import sys
+
+
+def getBinary(filename, outfile=None):
+    # get the binary file
+    if outfile is None:
+        outfile = sys.stdout
+
+    ftp.retrbinary("RETR " + "target.png", outfile.write)
+
+try:
+    ftp = FTP("ftp://ndrives.calu.edu/")
+    ftp.login(user = "hah5158", passwd = "Pbhmount18")
+except FTP.all_errors as e:
+    print "An error occured with the FTP connection\nError " + e
+
+getBinary("target.png")
