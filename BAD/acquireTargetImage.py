c5db363ac83f86ffb36a6321699cada4cd71125d
Paul Hahn <paulhahn18@gmail.com>
2016-03-28 17:08:17 -0400

    Update acquireTargetImage.py

--- BAD/acquireTargetImage.py
+++ BAD/acquireTargetImage.py
@@ -5,22 +5,31 @@ def getBinary(filename, outfile=None):
     try:
         count = 0
         data = []
+        
+        #initialize and connect to network driver file server.
         ftp = ftplib.FTP('ndrives.calu.edu')
         ftp.login(user = 'hah5158', passwd = 'Pbhmount18')
+        
+        #ftp.dir() will retrieve and print all available files/dirs
+        #data will contain this info instead to minimize output
         ftp.dir(data.append)
         
+        #Output only the valid files (.png)
         for item in data:
             if ".png" in item:
                 print "- " + item
                 count = count + 1
                 
+        #Notify user if there arent any valid files in directory
         if count <= 0:
             print "No appropriate images are on server"
             
         print "-----------------------------------"
-                
+        
+        #write file to CWD (BAD/BAD)
         ftp.retrbinary("RETR " + filename, open("target.png", 'wb').write)
         ftp.quit()
+        
     except ftplib.all_errors as e:
         print "An error occured with the FTP connection\nError " + str(e)
 
