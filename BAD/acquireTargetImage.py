0849c7feff5e12611169990793864c2b37f06360
Paul Hahn <paulhahn18@gmail.com>
2016-04-18 19:16:22 -0400

    Added functionality
    Added the ability to adjust to different target names
    
    Added ability to read the camera type from file

--- BAD/acquireTargetImage.py
+++ BAD/acquireTargetImage.py
@@ -1,32 +1,25 @@
 import ftplib
-def getDataFile(outfile=None, ftp):
-        try:
-        count = 0
-        data = []
-        #write file to CWD (BAD/BAD)
-        ftp.retrbinary("RETR " + filename, open("autogendata.txt", 'wb').write)
-        ftp.quit()
-        
-def getParameters():
-    filename = "autogendata.txt"
-    f = open(filename, 'r')
-    imgname = f.readline()
-    cameratype = f.readline()
-    temp = {"file":"", "camera":"")
-    return temp
+import sys
     
-def getBinary(filename, outfile=None):
+def getBinary(filename="target.png", addr="ndrives.calu.edu", username="hah5158", psk="Pbhmount18", outfile=None):
     
     try:
         count = 0
         data = []
         
         #initialize and connect to network driver file server.
-        ftp = ftplib.FTP('ndrives.calu.edu')
-        ftp.login(user = 'hah5158', passwd = 'Pbhmount18')
-        getDataFile(ftp)
-        data = getParamters()
-        cameratype = data["camera"]
+        ftp = ftplib.FTP(addr)
+        ftp.login(user = username, passwd = psk)
+
+        #write file to CWD (BAD/BAD)
+        genfile = "autogendata.txt"
+        ftp.retrbinary("RETR " + genfile, open(genfile, 'wb').write)
+        
+        f = open(genfile, 'r')
+        imgname = f.readline().rstrip()
+        cameratype = f.readline()
+
+        print "'"+imgname+"'"
         #ftp.dir() will retrieve and print all available files/dirs
         #data will contain this info instead to minimize output
         ftp.dir(data.append)
@@ -44,15 +37,16 @@ def getBinary(filename, outfile=None):
         print "-----------------------------------"
         
         #write file to CWD (BAD/BAD)
-        ftp.retrbinary("RETR " + filename, open("target.png", 'wb').write)
+        ftp.retrbinary("RETR " + imgname, open('target.png', 'wb').write)
         ftp.quit()
         
     except ftplib.all_errors as e:
         print "An error occured with the FTP connection\nError " + str(e)
-
+    return
+    
 
 
 # command line argument is filename on server
 print "CL Input " + str(len(sys.argv)) + " " + sys.argv[1]
 print "-----------------------------------"
-getBinary(sys.argv[1])
+getBinary(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
