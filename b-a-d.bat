4e68ed8e92996681f9da62b0deca0bab434d54a4
Paul Hahn <paulhahn18@gmail.com>
2016-04-22 17:26:51 -0400

    Update b-a-d.bat

--- b-a-d.bat
+++ b-a-d.bat
@@ -1,11 +1,28 @@
 @echo off
+ 
+echo  ______     ______     ______     ______     ______     __   __    
+echo /\  == \   /\  ___\   /\  __ \   /\  ___\   /\  __ \   /\ "-.\ \   
+echo \ \  __^<   \ \  __\   \ \  __ \  \ \ \____  \ \ \/\ \  \ \ \-.  \  
+echo  \ \_____\  \ \_____\  \ \_\ \_\  \ \_____\  \ \_____\  \ \_\\"\_\ 
+echo   \/_____/   \/_____/   \/_/\/_/   \/_____/   \/_____/   \/_/ \/_/ 
+echo.  
+echo  ______     __  __     ______   ______    
+echo /\  __ \   /\ \/\ \   /\__  _\ /\  __ \   
+echo \ \  __ \  \ \ \_\ \  \/_/\ \/ \ \ \/\ \  
+echo  \ \_\ \_\  \ \_____\    \ \_\  \ \_____\ 
+echo   \/_/\/_/   \/_____/     \/_/   \/_____/ 
+echo.  
+echo  _____     ______     ______     __   __     ______    
+echo /\  __-.  /\  == \   /\  __ \   /\ "-.\ \   /\  ___\   
+echo \ \ \/\ \ \ \  __^<   \ \ \/\ \  \ \ \-.  \  \ \  __\   
+echo  \ \____-  \ \_\ \_\  \ \_____\  \ \_\\"\_\  \ \_____\ 
+echo   \/____/   \/_/ /_/   \/_____/   \/_/ \/_/   \/_____/ 
 
 echo.
-echo BEACON AUTONOMOUS DRONE
-echo CALIFORNIA UNIVERISTY OF PA
-echo PAUL HAHN	
-echo JOHN DEROSA
-echo VINCENT VITOLO
+echo            Development Team:
+echo            ..PAUL HAHN	
+echo            ..JOHN DEROSA
+echo            ..VINCENT VITOLO
 echo.
 
 echo Starting Mission Planner Software
