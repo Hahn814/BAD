b6719be2eb7106d950e3fa8c090476ee711fd589
Paul Hahn <paulhahn18@gmail.com>
2016-04-23 12:39:19 -0400

    Update notify.py

--- BAD/notify.py
+++ BAD/notify.py
@@ -1,11 +1,21 @@
 import socket
+import time
+import subprocess
 
-#create an INET socket
+subprocess.call("/home/BAD/BAD/python WiFiSwitcher.py network ssid psk")
+
+# create an INET socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print "Attempting to notify Edison.."
 
 try:
-    s.connect(("192.168.2.27", 80))
-    print "Notification successful"
-except RuntimeError:
-    print "Notification failed.."
+    s.connect((socket.gethostname(), 2016))
+    print "<< Notification successful >>"
+
+except Exception as e:
+    print "<< Notification failed >>"
+    print e
+
+
+print("Program dialog will close..")
+time.sleep(5.5)
