45228ccc007790c1a2bb31b2739152d8b7cc8c9d
Paul Hahn <paulhahn18@gmail.com>
2016-04-23 12:45:59 -0400

    Update b-a-d.bat

--- b-a-d.bat
+++ b-a-d.bat
@@ -1,22 +1,12 @@
 @echo off
  
-echo  ______     ______     ______     ______     ______     __   __    
-echo /\  == \   /\  ___\   /\  __ \   /\  ___\   /\  __ \   /\ "-.\ \   
-echo \ \  __^<   \ \  __\   \ \  __ \  \ \ \____  \ \ \/\ \  \ \ \-.  \  
-echo  \ \_____\  \ \_____\  \ \_\ \_\  \ \_____\  \ \_____\  \ \_\\"\_\ 
-echo   \/_____/   \/_____/   \/_/\/_/   \/_____/   \/_____/   \/_/ \/_/ 
-echo.  
-echo  ______     __  __     ______   ______    
-echo /\  __ \   /\ \/\ \   /\__  _\ /\  __ \   
-echo \ \  __ \  \ \ \_\ \  \/_/\ \/ \ \ \/\ \  
-echo  \ \_\ \_\  \ \_____\    \ \_\  \ \_____\ 
-echo   \/_/\/_/   \/_____/     \/_/   \/_____/ 
-echo.  
-echo  _____     ______     ______     __   __     ______    
-echo /\  __-.  /\  == \   /\  __ \   /\ "-.\ \   /\  ___\   
-echo \ \ \/\ \ \ \  __^<   \ \ \/\ \  \ \ \-.  \  \ \  __\   
-echo  \ \____-  \ \_\ \_\  \ \_____\  \ \_\\"\_\  \ \_____\ 
-echo   \/____/   \/_/ /_/   \/_____/   \/_/ \/_/   \/_____/ 
+echo oooooooooo.  oooooooooooo       .o.         .oooooo.     .oooooo.   ooooo      ooo 
+echo `888'   `Y8b `888'     `8      .888.       d8P'  `Y8b   d8P'  `Y8b  `888b.     `8' 
+echo  888     888  888             .8"888.     888          888      888  8 `88b.    8  
+echo  888oooo888'  888oooo8       .8' `888.    888          888      888  8   `88b.  8  
+echo  888    `88b  888    "      .88ooo8888.   888          888      888  8     `88b.8  
+echo  888    .88P  888       o  .8'     `888.  `88b    ooo  `88b    d88'  8       `888  
+echo o888bood8P'  o888ooooood8 o88o     o8888o  `Y8bood8P'   `Y8bood8P'  o8o        `8 
 
 echo.
 echo            Development Team:
@@ -30,6 +20,9 @@ start /MIN /D"C:\Program Files (x86)\Mission Planner" MissionPlanner.exe
 
 echo Starting Target Collection Application 
 start /MIN /D"C:\Users\Paul\Documents\Visual Studio 2015\Projects\CollectTarget\CollectTarget\bin\Debug" CollectTarget.exe
+
+echo Starting Communication With Intel Edison
+start /MIN /D"C:\Users\Paul\BeaconDrone" notify.py
 
 pause
 exit
