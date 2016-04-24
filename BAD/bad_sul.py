5ded3f3c690cd0197ff37f1ab1a984d3e63cb5fc
Paul Hahn <paulhahn18@gmail.com>
2016-04-23 20:51:32 -0400

    Update bad_sul.py

--- BAD/bad_sul.py
+++ BAD/bad_sul.py
@@ -1,21 +1,27 @@
 import socket
 import subprocess
 
-subprocess.call("/home/BAD/BAD/python WiFiSwitcher.py network ssid psk")
-# Create an INET servee socket
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+PORT_NUMBER = 2016
 
+subprocess.call(['python','WiFiSwitcher.py','network', 'ssid', 'psk'])
+# Create an INET servee socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+HOST_NAME =  socket.gethostbyname('0.0.0.0')
 # Bind socket to port and host
 # bind (address, port)
-serversocket.bind(('', 2016))
+serversocket.bind((HOST_NAME, PORT_NUMBER))
 
+print "Listening on port " +  str(PORT_NUMBER) + " for startup message.. " + str(HOST_NAME)
 # Listen for a single connection from the GCU
-serversocket.listen(1)
 
 # accept incoming client connections
-(clientsocket, addr) = serversocket.accept()
 
-print "Start up message recieved: " + str(clientsocket.getsockname)
+(data, addr) = serversocket.recvfrom(1024)
+
+print "Start up message recieved: " + data
 print "Starting main execution loop.."
 
-subprocess.call("/home/BAD/BAD/./bad-init.sh")
+print "Un comment the below section you ninny"
+#process = subprocess.Popen('./bad-init.sh', shell=True, stdout=subprocess.PIPE)
+#process.wait()
+#print process.returncode
