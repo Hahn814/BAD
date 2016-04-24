082c78cac90b963e155ee372ff3caa3bb0b01db0
Paul Hahn <paulhahn18@gmail.com>
2016-04-23 20:49:28 -0400

    Update notify.py

--- BAD/notify.py
+++ BAD/notify.py
@@ -2,13 +2,16 @@ import socket
 import time
 import subprocess
 
+SERVER_IP = '192.168.2.25'
+PORT_NUMBER = 2016
 # create an INET socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 print "Attempting to notify Edison.."
 
 try:
-    s.connect((socket.gethostname(), 2016))
+    s.connect((SERVER_IP, 2016))
     print "<< Notification successful >>"
+    s.send('START')
 
 except Exception as e:
     print "<< Notification failed >>"
