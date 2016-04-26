09848fa9e21eeafae0bd567eb1a6a8db848ec614
Paul Hahn <paulhahn18@gmail.com>
2016-04-26 11:59:34 -0400

    Update bad_sul.py

--- BAD/bad_sul.py
+++ BAD/bad_sul.py
@@ -3,6 +3,7 @@ import subprocess
 
 PORT_NUMBER = 2016
 
+# Command Line: python WiFiSwitcher.py network ssid psk
 subprocess.call(['python','WiFiSwitcher.py','network', 'ssid', 'psk'])
 # Create an INET servee socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -12,16 +13,14 @@ HOST_NAME =  socket.gethostbyname('0.0.0.0')
 serversocket.bind((HOST_NAME, PORT_NUMBER))
 
 print "Listening on port " +  str(PORT_NUMBER) + " for startup message.. " + str(HOST_NAME)
-# Listen for a single connection from the GCU
 
-# accept incoming client connections
-
+# accept incoming client message
 (data, addr) = serversocket.recvfrom(1024)
 
 print "Start up message recieved: " + data
 print "Starting main execution loop.."
 
-print "Un comment the below section you ninny"
-#process = subprocess.Popen('./bad-init.sh', shell=True, stdout=subprocess.PIPE)
-#process.wait()
-#print process.returncode
+# run the shell command script to begin program execution. 
+process = subprocess.Popen('./bad-init.sh', shell=True, stdout=subprocess.PIPE)
+process.wait()
+print process.returncode
