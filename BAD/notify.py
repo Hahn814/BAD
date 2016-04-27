36c5801e854a358a32c384ffe8cd808ea10f6be3
Paul Hahn <paulhahn18@gmail.com>
2016-04-26 22:02:46 -0400

    Update notify.py

--- BAD/notify.py
+++ BAD/notify.py
@@ -2,13 +2,16 @@ import socket
 import time
 import subprocess
 
+# These should be an adjustable variable in the future
 SERVER_IP = '192.168.2.25'
 PORT_NUMBER = 2016
-# create an INET socket
+
+# create the INET socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 print "Attempting to notify Edison.."
 
 try:
+    # connect and push a message to start
     s.connect((SERVER_IP, 2016))
     print "<< Notification successful >>"
     s.send('START')
